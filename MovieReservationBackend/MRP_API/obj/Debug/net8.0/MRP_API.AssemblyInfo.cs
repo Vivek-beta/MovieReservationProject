fa06f0552588bfcfc9b149e05257cbb9c54de459
Vivek Sasi Kumar <vivek.sasi.kumar@ibm.com>
2025-08-04 22:21:49 +0530

    Added Database and UI

--- MovieReservationBackend/MRP_API/obj/Debug/net8.0/MRP_API.AssemblyInfo.cs
+++ MovieReservationBackend/MRP_API/obj/Debug/net8.0/MRP_API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MRP_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15106b580db2f36558491a9f6312e3ef63c863b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("MRP_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MRP_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
